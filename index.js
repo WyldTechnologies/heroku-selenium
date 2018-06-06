127c127da748de2d40c762ac7b32b9edf9b2428c
raheel <v-zenq@finagraph.com>
2018-06-06 11:36:35 +0530

    updated with Mocha's test structure

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-var webdriver = require('selenium-webdriver');
-var express = require('express')
-var app = express()
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-var port = process.env.PORT || 14000;
-var By = webdriver.By;
-
-var chromeCapabilities = webdriver.Capabilities.chrome();
-var chromeOptions = {
-    //'args': ['--test-type', '--start-maximized']
-	'args': ['--headless']
-};
-chromeCapabilities.set('chromeOptions', chromeOptions);
-
-app.get('/test', function (req, res) {
-    var driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build();
-    driver.get('http://demo0.wyldmesh.net:8001/comms');
-    driver.wait(function() {
-        return driver.getTitle().then(function(title) {
-            console.log('> ',title);
-            return title === 'Wyld Demo: Comms';
-        });
-    }, 10000).then(function() {
-        res.status(200).send('Done');
-    }, function(error) {
-        res.status(200).send(error);
-    });
-    driver.quit();
-});
-
-app.listen(port, function () {
-    console.log('Example app listening on port: ',port)
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
