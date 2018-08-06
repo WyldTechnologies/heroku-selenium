f3a06f0acb523bd7e0b8432ce77b73532c483710
raheel <raheel.sm@gmail.com>
2018-08-06 21:38:45 +0530

    attempt 4 - running on docker

--- protractor/conf.js
+++ protractor/conf.js
@@ -1,7 +1,8 @@
 // conf.js
 exports.config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  directConnect: true,
   specs: ['./specs/*.js'],
   capabilities: {
     browserName: 'chrome',
